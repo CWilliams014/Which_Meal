fba8502358b46b6fd6a6e9cc0057a4dfb40dedc3
Cole Williams <cowi4030@colorado.edu>
2016-10-04 21:48:57 -0700

    not able to export itemInfo array. Linting done on controller

--- server/controllers/getAllRestaurants.js
+++ server/controllers/getAllRestaurants.js
@@ -3,55 +3,32 @@
 import allRestaurantTitles from '../../data/ListOfRestaurantNames'
 const cheerio = require('cheerio');
 const request = require('request');
-
+// currently working just for whataburger for now
 const getRestaurants = (req, res, next) => {
-
-
-
 	const url = 'http://www.shapefit.com/restaurants/whataburger-calories.html'
 
-	request(url,(error, response, html) => {
-		if(error) console.log('error on cheerio request', error)
+	request(url, (error, response, html) => {
+		if (error) console.log('error on cheerio request', error)
 		let $ = cheerio.load(html, {
-      	normalizeWhitespace: true
-      });
+			normalizeWhitespace: true
+		});
 
 		let categories = [];
-		let rowData = []
+		let foodItemInfo = []
 
 		let columnName = $('table th').each((index, element) => {
 			categories.push(cheerio(element).text())
-			
-		})		
-			$('tbody tr').each((index, element) => {
-				var children = $(element).children()
-				var o = {}
-				$(children).each((index, element) => {
-					o[categories[index]] = cheerio(element).text()
-					rowData.push(o)
-				})
-				
-			})
-
-		// console.log('categories', categories)
-		console.log(';LKJ;LKJ', rowData)
-
-
 
 		})
+		$('tbody tr').each((index, element) => {
+			var children = $(element).children()
+			var o = {}
+			$(children).each((index, element) => {
+				o[categories[index]] = cheerio(element).text()
+				foodItemInfo.push(o)
+			})
+		})
+	})
 }
 
-export default getRestaurants
-
-
-
-// {
-// 	Restaurant: {
-// 		name: 'mcdonalds',
-// 		items: {
-// 			burger: {
-// 				calories: 1000,
-// 				carbs: 123,
-// 			}
-// 		}
-// }
+module.exports =  {getRestaurants, foodItemInfo}
