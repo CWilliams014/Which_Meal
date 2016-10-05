59be6b519ec4ba2f3b6726a6452f45c89d3d3158
Cole Williams <cowi4030@colorado.edu>
2016-10-04 21:28:09 -0700

    data structure figured out for parsing restaurant info

--- server/controllers/getAllRestaurants.js
+++ server/controllers/getAllRestaurants.js
@@ -1,38 +1,57 @@
 'use strict';
 
+import allRestaurantTitles from '../../data/ListOfRestaurantNames'
 const cheerio = require('cheerio');
 const request = require('request');
 
 const getRestaurants = (req, res, next) => {
-	const url = 'http://fastfoodnutrition.org/'
+
+
+
+	const url = 'http://www.shapefit.com/restaurants/whataburger-calories.html'
 
 	request(url,(error, response, html) => {
 		if(error) console.log('error on cheerio request', error)
-
 		let $ = cheerio.load(html, {
       	normalizeWhitespace: true
       });
 
-		let restaurantTitles = [];
+		let categories = [];
+		let rowData = []
 
-		var listItem = $('div.container.main_container div div.hidden-sm.hidden-xs.left_column ul.list  li').each((index, element) => {
-			restaurantTitles.push(cheerio(element).text())
-			console.log(cheerio(element).text())
-		})
-		// res.json(restaurantTitles)
-		let restaurantItems = []
-		let nextURL;
-
-		for(var i = 0; i < restaurantTitles.length; i++) {
-			console.log('in for loop')
-			request(url + restaurantTitles[i], (error, response, html) => {
-				let foodItem = $('ul.col-xs-12.col-sm-6.rest_item_list li a').each((index, element) => {
-					console.log('FOODITEM', element)
+		let columnName = $('table th').each((index, element) => {
+			categories.push(cheerio(element).text())
+			
+		})		
+			$('tbody tr').each((index, element) => {
+				var children = $(element).children()
+				var o = {}
+				$(children).each((index, element) => {
+					o[categories[index]] = cheerio(element).text()
+					rowData.push(o)
 				})
+				
 			})
-		}
 
-	})
+		// console.log('categories', categories)
+		console.log(';LKJ;LKJ', rowData)
+
+
+
+		})
 }
 
 export default getRestaurants
+
+
+
+// {
+// 	Restaurant: {
+// 		name: 'mcdonalds',
+// 		items: {
+// 			burger: {
+// 				calories: 1000,
+// 				carbs: 123,
+// 			}
+// 		}
+// }
