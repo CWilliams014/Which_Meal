4c2760404e6ffd400d7fa0a6d5c8bc7f307b0b64
Cole Williams <cowi4030@colorado.edu>
2016-10-05 16:12:19 -0700

    helper functions folder added

--- client/components/menu/MenuDisplay.js
+++ client/components/menu/MenuDisplay.js
@@ -2,20 +2,12 @@ import React from 'react'
 import Entrees from './Entrees'
 
 const MenuDisplay = React.createClass ({
-
-	mapObject(object, callback) {
-	  return Object.keys(object).map(function (key) {
-	    return callback(key, object[key]);
-	  });
-	},
 	render() {
 		let menu = []
 		console.log('MENU', menu)
 		return (
 			<div>
-				{this.mapObject(this.props.menu, function (key, value) {
-  				return <div key={key+value}> {key} : {value}</div>;
-					})}
+
 			</div>
 		)
 	}
