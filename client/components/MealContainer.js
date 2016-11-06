9651dceed45be3c1d1bc4539c51a46410eeca71d
Cole Williams <cowi4030@colorado.edu>
2016-11-06 12:40:05 -0800

    Search bar live update funtionality working properly, yay

--- client/components/MealContainer.js
+++ client/components/MealContainer.js
@@ -26,40 +26,43 @@ class MealWrapper extends React.Component {
         this.getRestaurantData = this.getRestaurantData.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
     }
-    selectRestaurant(e) {
-      let chosen = e.target.name
-      this.setState({selectedRestaurant : chosen})
-      this.getRestaurantData(chosen)
-    }
+    
+  selectRestaurant(e) {
+    let chosen = e.target.name
+    this.setState({selectedRestaurant : chosen})
+    this.getRestaurantData(chosen)
+  }
 
-    handleSearch(e) {
-      console.log('handleSearchh', e.target.value)
-      this.setState({searchTerm: e.target.value })
-    }
+  handleSearch(e) {
+    console.log('handleSearchh', e.target.value)
+    this.setState({searchTerm: e.target.value })
+  }
 
-    getRestaurantData(name) {
-      this.setState({loading: true})
-      let _name = name
-      return axios.get('/restaurants', {params: { id: _name}}).then((response) => {
-        this.setState({loading: false})
-        let data = response.data
-        console.log('getrest data', data)
-        let splicedData = data.splice(0,1)
-        this.setState({currentMenu: response.data})
-      })
-    }
+  getRestaurantData(name) {
+    this.setState({loading: true})
+    let _name = name
+    return axios.get('/restaurants', {params: { id: _name}}).then((response) => {
+      this.setState({loading: false})
+      let data = response.data
+      console.log('getrest data', data)
+      let splicedData = data.splice(0,1)
+      this.setState({currentMenu: response.data})
+    })
+  }
+  
   render() {
     const _this = this;
-    let menu;
-    menu = this.state.currentMenu.filter(function(item, index) {
-        console.log('item', item.itemName.toLowerCase())
-        console.log('stolower', _this.state.searchTerm.toLowerCase())
-        return item.itemName.toLowerCase().indexOf(_this.state.searchTerm.toLowerCase() >= 0)
-      }) || []
-    
-    console.log('menu', menu)
-
-
+    var menu;
+    var newMenu = []
+    this.state.currentMenu.filter(function(item, index) {
+      let currItem = item.itemName.toLowerCase()
+      let currSearch = _this.state.searchTerm.toLowerCase()
+        
+      if(currItem.includes(currSearch)) {
+        newMenu.push(item)
+        console.log('newwwwww menu', newMenu)
+      }
+    }) 
   return (
       <div className="meal-container">
         <Dropdown selectRestaurant={this.selectRestaurant}
@@ -73,7 +76,7 @@ class MealWrapper extends React.Component {
                      sortColumn={this.sortColumn}
                      handleSearch={this.handleSearch}
                      searchTerm={this.state.searctTerm}
-                     menu={menu} />
+                     menu={newMenu} />
       </div>
     )
   }
